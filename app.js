370faab27fefdbb18dec4a80e77a5bfa47b5c8da
mike <mclancy96@gmail.com>
2020-11-11 04:03:31 +0000

    finished logic for basic random generation

--- app.js
+++ app.js
@@ -10,11 +10,40 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 
 app.get("/", function(req,res){
-	res.render("index");
-});
-
-app.post("/", function(req,res){
-	res.redirect("/", {hold:hold})
+	const holdAreas = ["MC", "SD", "DRY", "CLR", "FZR"]
+	const holdSections = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+	
+	var numAreas = 4
+	
+	var randArea = random.int(0,numAreas)
+	var setArea = holdAreas[randArea]
+	
+	if (setArea == "MC"){
+		var randSection = random.int(0,25)
+		var setSection = holdSections[randSection]
+	} else if (setArea == "SD"){
+		var randSection = random.int(0,17)
+		var setSection = holdSections[randSection]
+	} else if (setArea == "DRY"){
+		var randSection = random.int(0,4)
+		var setSection = holdSections[randSection]
+	} else if (setArea == "CLR"){
+		var randSection = random.int(0,3)
+		var setSection = holdSections[randSection]
+	} else if (setArea == "FZR"){
+		var randSection = random.int(0,3)
+		var setSection = holdSections[randSection]
+	}
+	
+	var setNumber = random.int(1,120)
+	
+	var hold = {
+		area: setArea,
+		section: setSection,
+		number:  setNumber
+	}
+	var test = random.int(0,4)
+	res.render("index", {hold:hold});
 });
 
 app.listen(process.env.PORT, function(){
